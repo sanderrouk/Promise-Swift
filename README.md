93d51c50934a3a85692e3bcbb137e2fff3d07b72
Sander Rõuk <sander@rouk.io>
2020-04-19 14:49:10 +0300

    Update documentation and licence

--- README.md
+++ README.md
@@ -1,4 +1,95 @@
-# Promise iOS
-
+# Promise
 ![Linux Build and Test](https://github.com/sanderrouk/Promise-iOS/workflows/Linux%20Build%20and%20Test/badge.svg)  ![MacOS Build and Test](https://github.com/sanderrouk/Promise-iOS/workflows/MacOS%20Build%20and%20Test/badge.svg)
 
+Promise is a simple Swift framework that works with iOS, MacOS and Linux. Promise provides a simple way for synchronization. The purpose of this framework is to provide a simple lightweight implementation of Promises for Swift. If you are looking for a more robust implementation then we recommend [Google's Promises](https://github.com/google/promises) which are very robust and fairly light weight. This framework was built to be included in iOS PowerTools. A benefit this framework brings is that the entirety of the implementation is very lightweight and if desired can be added to a project without any dependency managers making the footprint smaller.
+
+## Installation
+
+### Using the Source Files
+A very lightweight way to use this is to navigate to the `Sources` directory and add all of the files from there to your project. This however does come with a caveat that the code will not be updated automatically in case of new versions. For a dependency manager approach follow one of the following methods.
+
+### Swift Package Manager (All platforms)
+As of Xcode 11 SPM integrates nicely with Xcode. This means that installing dependencies with Xcode support is super easy. To add the dependency using Swift Package Manager do the following steps:
+
+1. Select the desired project and choose the `Swift Packages` tab of your project.
+2. Tap on the + button.
+3. Enter `https://github.com/sanderrouk/Promise-Swift` onto the search bar and click next.
+4. Choose the `Version` option leaving the selection on `Up to Next Major` option.
+5. Click Next.
+6. Click Finish.
+7. Either create a separate file for it or add `import Promise` in the file where you want to use it.
+
+### Carthage (MacOS or iOS)
+1. Add `github "sanderrouk/Promise-Swift" ~> 1.0.0` project to your Cartfile.
+2. Run `$ carthage update`
+3. [Do the additional steps required for carthage frameworks.](https://github.com/Carthage/Carthage#adding-frameworks-to-an-application)
+4. Either create a separate file for it or add `import PinKit` in the file where you want to use it.
+
+## Usage
+Promise works just like any other promises framework making the syntax very familiar.
+
+```swift
+        let promise = Promise<String>()
+            .always {
+                print("Always called")
+        }
+
+        promise.cancellationHandler = self
+
+        let secondPromise = promise.then({ string -> Bool in
+            throw GenericError.random
+            print(string)
+            return true
+        })
+
+        let thirdPromise = secondPromise.then({
+            return $0 ? "Hell yes" : "Hell nah"
+        })
+
+        thirdPromise.then({
+            print($0)
+        })
+        thirdPromise.cancel()
+
+/// These two can't be called on the same promise
+        promise.resolve(value: "Initial value")
+//        promise.reject(error: GenericError.random)
+
+        promise.catch({ _ in
+            print("First promise catch")
+        })
+
+        secondPromise.catch({ _ in
+            print("Second promise catch")
+        })
+
+        thirdPromise.catch({ _ in
+            print("Third promise catch")
+        })
+
+
+        let exampleJson =
+
+"""
+{
+    "value": "Some string"
+}
+""".data(using: .utf8)!
+
+        Promise { fulfill, reject in
+            do {
+                let object = try JSONDecoder().decode(SomeDecodable.self, from: exampleJson)
+                fulfill(object.value)
+            } catch {
+                reject(error)
+            }
+        }
+        .then({ print($0) })
+        .catch({ print($0.localizedDescription) })
+        .always {
+            print("Called always on standalone function")
+        }
+```
+
+## License
+The project is under the MIT licence meaning you can use this project however you want.
--- Sources/Promise/Promise.swift
+++ Sources/Promise/Promise.swift
@@ -1,34 +1,69 @@
+/*
+Copyright © 2020 Rouk OÜ. All rights reserved.
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 import Foundation
 
+/// A construct that an object needs to implement if it wants to listen to a cancellation from a promise.
 public protocol PromiseCancellationHandler: AnyObject {
+    
+    /// Method that will be called if a promise is cancelled.
     func cancel()
 }
 
+/// A construct for supporting asynchronous tasks. This construct does not handle any threading itself. This must be handled by the callers and consumers.
 public final class Promise<T> {
 
+    /// A construct representing the state of the promise.
     public enum State<T> {
+        
+        /// A pending state of the promise. If a promise has this state then it has not been resolved yet and has not called any callbacks.
         case pending
+        
+        /// A resolved state of the promise. If a promise has this state then it has been resolved and has called the appropriate callbacks to the result.
         case resolved(Result<T, Error>)
     }
 
+    /// A cancellation handler object. If an object needs to listen to the cancellation of a promise then it should be assigned to this variable.
     public weak var cancellationHandler: PromiseCancellationHandler?
-    public var state: State<T> = .pending
+    
+    /// The promise's state.
+    public private(set) var state: State<T> = .pending
 
     private var onSuccessCallbacks = [(T) -> Void]()
     private var onErrorCallbacks = [(Error) -> Void]()
 
+    /// A init that can be used a pending promise.
     public init() {
         self.state = .pending
     }
 
+    /// A init that can be used to create a promise with a resolved state.
+    /// - Parameter value: The resolved value of the state.
     public init(value: T) {
         self.state = .resolved(.success(value))
     }
 
+    /// A init that can be used to create a promise with a resolved state.
+    /// - Parameter error: The resolved error of the state.
     public init(error: Error) {
         self.state = .resolved(.failure(error))
     }
 
+    /// A init that can be used to create a pending promise.
+    /// - Parameter resolver: A resolver that resolves the promise.
     public init(
         _ resolver: @escaping (
         _ onFulfill: @escaping (T) -> Void,
@@ -41,6 +76,9 @@ public final class Promise<T> {
         }
     }
 
+    /// A method to add a block of code to be executed when a promise is completed.
+    /// - Parameter onFulfill: A block of code that gets called when a promise is resolved with a `.success(T)` result. This block of code returns  a new Promise.
+    /// - Returns: A new promise of type `Promise<U>` where `U` is dependant on the execution block.
     @discardableResult
     public func then<U>(_ onFullfill: @escaping (T) throws -> Promise<U>) -> Promise<U> {
         let newPromise = Promise<U>()
@@ -63,6 +101,9 @@ public final class Promise<T> {
         return newPromise
     }
 
+    /// A method to add a block of code to be executed when a promise is completed.
+    /// - Parameter onFulfill: A block of code that gets called when a promise is resolved with a `.success(T)` result.
+    /// - Returns: A new promise of type `Promise<U>` where `U` is dependant on the execution block.
     @discardableResult
     public func then<U>(_ onFullfill: @escaping (T) throws -> U) -> Promise<U> {
         return then({ value in
@@ -74,6 +115,9 @@ public final class Promise<T> {
         })
     }
 
+    /// A method to add a block of code to be executed when a promise is resolved with a failed state.
+    /// - Parameter onReject: A block of code that gets called when a promise is resolved with a `.failure(Error)` result. This block of code returns  a new Promise.
+    /// - Returns: The the promise this method was called on.
     @discardableResult
     public func `catch`(_ onReject: @escaping (Error) -> Void) -> Self {
         onErrorCallbacks.append(onReject)
@@ -81,6 +125,9 @@ public final class Promise<T> {
         return self
     }
 
+    /// A method used to resolve a promise with a value.
+    /// This method expects the promise not to be resolved already.
+    /// - Parameter value: The value that the promise is resolved with.
     public func resolve(value: T) {
         if case .resolved = state {
             assertionFailure("Promise already resolved")
@@ -90,6 +137,9 @@ public final class Promise<T> {
         resolveCallbacksIfNecessary()
     }
 
+    /// A method used to resolve a promise with an error.
+    /// This method expects the promise not to be resolved already.
+    /// - Parameter error: The error that the promise is resolved with.
     public func reject(error: Error) {
         if case .resolved = state {
             assertionFailure("Promise already resolved")
@@ -99,6 +149,9 @@ public final class Promise<T> {
         resolveCallbacksIfNecessary()
     }
 
+    /// A method to add a block of code to be executed when a promise is resolved with any state.
+    /// - Parameter callback: A block of code that gets called when a promise is resolved with any state.
+    /// - Returns: The the promise this method was called on.
     @discardableResult
     public func always(_ callback: @escaping () -> Void) -> Self {
         onSuccessCallbacks.append({ _ in callback() })
@@ -130,6 +183,7 @@ public final class Promise<T> {
 
 extension Promise: PromiseCancellationHandler {
 
+    /// A method that can be called to cancel a promise.
     public func cancel() {
         cancellationHandler?.cancel()
         clearCallbacks()
